487d4b781636f49ee724ea772f26efa1239e994a
aghfairawan <58446116+aghfairawan@users.noreply.github.com>
2023-10-27 23:58:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -34,16 +34,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const rateLimit = require("express-rate-limit");
 
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15, 
-});
-
-
-app.use("/auth", limiter); 
 
 app.use(databaseMidleware)
 
