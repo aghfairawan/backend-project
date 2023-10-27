bcc1fb1acc8c4c6b6a7915483c443835249942d3
aghfairawan <58446116+aghfairawan@users.noreply.github.com>
2023-10-28 00:28:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const authRouter = require('./routes/auth-route.js')
 const taskRouter = require('./routes/task-route.js')
 const authMiddleware = require('./midleware/authentication-midlware.js')
 const helmet = require("helmet");
-const cors = require('cors');
+
 
 
 
@@ -15,24 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(helmet());
 
-const allowedOrigins = ['http://localhost:5173'];
 
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      
-      callback(null, true);
-    } else {
-      
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  
-};
-
-
-app.use(cors(corsOptions));
 
 
 
