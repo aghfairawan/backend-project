3902d3d2173177223aef88bfe8062a076a1009b9
aghfairawan <58446116+aghfairawan@users.noreply.github.com>
2023-10-30 14:08:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(helmet());
 
-const allowedOrigins = ['https://aghfa-project-fullstack.firebaseapp.com'];
+const allowedOrigins = ['https://aghfa-project-fullstack.web.app'];
 
 
 const corsOptions = {
